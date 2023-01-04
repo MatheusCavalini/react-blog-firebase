8112445ef67bc21662017379b0c1b0a59a2a1552
MatheusCavalini <matheuscav20@gmail.com>
2023-01-03 22:51:59 -0300

    Criação de área de login e cadastro

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react"
+
+import styles from './Register.module.css'
+
+const Register = () => {
+
+    const [displayName, setDisplayName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setError("")
+
+        const user = {
+            displayName,
+            email,
+            password
+        }
+
+        if(password !== confirmPassword){
+            setError("As senhas precisam ser iguais.")
+            return
+        }
+
+        console.log(user)
+
+    }
+
+    return (
+        <div className={styles.register}>
+            <h1>Cadastre-se para postar</h1>
+            <p>Crie seu usuário e compartilhe suas histórias</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Nome:</span>
+                    <input 
+                        type="text" 
+                        name="displayName" 
+                        required 
+                        placeholder="Nome do usuário" 
+                        value={displayName}
+                        onChange={(e) => setDisplayName(e.target.value)}/>
+                </label>
+                <label>
+                    <span>E-mail:</span>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        required 
+                        placeholder="E-mail do usuário" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha:</span>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        required 
+                        placeholder="Insira sua senha" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Confirmar Senha:</span>
+                    <input 
+                        type="password" 
+                        name="confirmPassword" 
+                        required 
+                        placeholder="Confirme sua senha" 
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}/>
+                </label>
+                <button type="submit" className="btn">Cadastrar</button>
+                {error && <p className="error">{error}</p>}
+            </form>
+        </div>
+    )
+}
+
+export default Register
