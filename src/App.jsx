34b996b5efb1555c34c6e5ffac8469c1a5099fca
MatheusCavalini <matheuscav20@gmail.com>
2023-01-04 20:13:11 -0300

    Contexto de autenticação e acl navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 //pages
 import Home from './pages/Home/Home';
@@ -10,21 +15,47 @@ import Footer from './components/Footer';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
+// context 
+import { AuthProvider } from './context/AuthContext'
+import Dashboard from './pages/Dashboard/Dashboard';
+import CreatePost from './pages/CreatePost/CreatePost';
+
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication() 
+
+  const loadingUser = user === undefined
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      console.log(user)
+      setUser(user)
+    })
+  },[auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/register' element={<Register />}/>
-            <Route path='/about' element={<About />}/>
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/posts/create' element={<CreatePost />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
